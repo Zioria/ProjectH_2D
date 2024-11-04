775e6bc645ef7ab1dba0d31011df6888341d4174
ichanawat <ishanawat.bunk@gmail.com>
2024-11-04 13:50:04 +0700

    Add RemoveItem

--- ProjectH_2D/Assets/Script/Inventorymanager.cs
+++ ProjectH_2D/Assets/Script/Inventorymanager.cs
@@ -11,6 +11,10 @@ public class Inventorymanager : MonoBehaviour
     public Transform ItemContent;
     public GameObject InventoryItem;
 
+    public Toggle EnableRemove;
+
+    public InventoryitemController[] InVentoryItems;
+
     public void Awake()
     {
         Instance = this;
@@ -28,32 +32,55 @@ public class Inventorymanager : MonoBehaviour
     
     public void ListItems()
     {
+        foreach (Transform item in ItemContent)
+        {
+            Destroy(item.gameObject);
+        }
+
         foreach (var item in Items)
         {
             GameObject obj = Instantiate(InventoryItem, ItemContent);
             var itemName = obj.transform.Find("ItemName").GetComponent<Text>();
             var itemIcon = obj.transform.Find("ItemIcon").GetComponent<Image>();
+            var removeButton = obj.transform.Find("Removeitem").GetComponent<Button>();
 
             itemName.text = item.itemName;
             itemIcon.sprite = item.icon;
+
+            if(EnableRemove.isOn)
+            {
+                removeButton.gameObject.SetActive(true);
+            }
+                
+            
         }
     }
 
-    //public void EnableItemsRemove()
-   // {
-        //if(EnableRemove.isOn)
-        //{
-           // foreach (Transform item in ItemContent)
-            //{
-              //  item.Find("RemoveButton").gameObject.SetActive(true);
-           // }
-       // }
-       // else
-       // {
-           // foreach (Transform item in ItemContent)
-           // {
-            //    item.Find("RemoveButton").gameObject.SetActive(false);
-           // }
-      //  }
-   // }
+    public void EnableItemsRemove()
+    {
+        if(EnableRemove.isOn)
+        {
+            foreach (Transform item in ItemContent)
+            {
+                item.Find("Removeitem").gameObject.SetActive(true);
+            }
+        }
+        else
+        {
+            foreach (Transform item in ItemContent)
+            {
+                item.Find("Removeitem").gameObject.SetActive(false);
+            }
+        }
+    }
+
+    public void SetInventoryItems()
+    {
+        InVentoryItems = ItemContent.GetComponentsInChildren<InventoryitemController>();
+
+        for (int i = 0; i < Items.Count; i++)
+        {
+            InVentoryItems[i].AddItem(Items[i]);
+        }
+    }
 }
