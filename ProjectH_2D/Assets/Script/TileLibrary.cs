57fbae738bac37266d1f4daabcd78861f1971f58
Zioria <offjek065@gmail.com>
2024-11-07 19:58:07 +0700

    DeleteAll

--- ProjectH_2D/Assets/Script/TileLibrary.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-using UnityEngine.Tilemaps;
-
-[CreateAssetMenu(fileName = "TileLibrary", menuName = "Tile/TileLibrary")]
-public class TileLibrary : ScriptableObject
-{
-    public Tile[] tiles;  // Array to store different tiles
-}
